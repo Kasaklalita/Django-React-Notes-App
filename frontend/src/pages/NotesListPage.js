db6cda4c6dd590bb321183bdf5cc058cc6013aec
Veniamin Polienko <kasaklalita@mail.ru>
2022-11-19 16:25:40 +0300

    Finished frontend

--- frontend/src/pages/NotesListPage.js
+++ frontend/src/pages/NotesListPage.js
@@ -1,20 +1,39 @@
-import React, { userState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
+import ListItem from '../components/ListItem'
+import AddButton from '../components/AddButton'
+
 
 const NotesListPage = () => {
-  let getNotes = async () => {
-    let response = await fetch("http://127.0.0.1:8000/api/notes/");
-    let data = await response.json();
-    console.log("DATA:", data);
-    setNotes(data);
-  };
 
-  let [notes, setNotes] = userState([]);
+    let [notes, setNotes] = useState([])
 
-  useEffect(() => {
-    getNotes();
-  }, []);
+    useEffect(() => {
+        getNotes()
+    }, [])
 
-  return <div>notes</div>;
-};
 
-export default NotesListPage;
+    let getNotes = async () => {
+
+        let response = await fetch('/api/notes/')
+        let data = await response.json()
+        setNotes(data)
+    }
+
+    return (
+        <div className="notes">
+            <div className="notes-header">
+                <h2 className="notes-title">&#9782; Notes</h2>
+                <p className="notes-count">{notes.length}</p>
+            </div>
+
+            <div className="notes-list">
+                {notes.map((note, index) => (
+                    <ListItem key={index} note={note} />
+                ))}
+            </div>
+            <AddButton />
+        </div>
+    )
+}
+
+export default NotesListPage
--- mynotes/mynotes/settings.py
+++ mynotes/mynotes/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-3y!%$=%)e$dpv_-vi-kg)^+!&3)4swbyizd@0_c*-kauuf*@83
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+
+]
 
 
 # Application definition
@@ -38,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mynotes.urls'
@@ -124,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
